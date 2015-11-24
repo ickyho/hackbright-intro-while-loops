d69a0f40217feebca55eb30c18316fb8c8c78d0c
ickyho <katherine.broner@gmail.com>
2015-11-23 20:02:41 -0800

    odd numbers

--- 01-while-loops/while_fun_loops.py
+++ 01-while-loops/while_fun_loops.py
@@ -1,5 +1,18 @@
+#i = 0
+#while(i <= 20):
+#	if (i == 13):
+#		print "Hello"
+#	else:
+#		print i 
+#	i += 1 
+
+#i = 0
+#while (i <= 100):
+#	print i 
+#	i += 10 
+
 i = 0
-while(i <= 20):
-	print i 
-	i += 1 
-	
+while (i <= 10):
+	if (i % 2 == 1):
+		print i
+	i += 1
